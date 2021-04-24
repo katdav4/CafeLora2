4760911d08cf631fb6fa85d6a320ec91396af357
Katerina Davidova <kat@moucha.cc>
2021-04-24 13:38:17 +0200

    vyjizdej a zajizdej menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('#nav');
+const navIElm = document.querySelectorAll('#nav-i');
+
+navBtnElm.addEventListener('click', (e) => {
+  navElm.classList.toggle('nav-closed');
+});
+
+navIElm.forEach((items) => {
+  items.addEventListener('click', (e) => {
+    navElm.classList.toggle('nav-closed');
+  });
+});
